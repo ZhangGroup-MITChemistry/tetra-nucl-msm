29f3cf1b2a8fef00cf92c4a465e78827a5f460e8
Shuming Liu <smliu@mit.edu>
2022-10-24 12:31:52 -0400

    add extended snapshot

---
+++ simulations-nrl-172/extend-init-snapshot/pick_extended_snapshot.sh
@@ -0,0 +1,1 @@
+/home/xclin/bin/anaconda2/bin/mdconvert -o extended.dcd -i 3 /nfs/pool002/users/smliu/tetra-nucl-msm/simulations-nrl-172/extend-init-snapshot/extend-init-snapshot-dist/sim/run000/DUMP_FILE.dcd
